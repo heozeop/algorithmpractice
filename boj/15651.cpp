f79b2a2128aafbb23ca303ba4f7d719c8a6abab6
LeeSeungChan <dltmdcks702@gmail.com>
2020-01-11 15:23:08 +0900

    boj 15651

---
+++ boj/15651.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+vector<int> picked;
+void findOne(int cnt, vector<int> picked) {
+    if (cnt == m) {
+        for (int i = 0; i < picked.size(); i++) {
+            cout << picked[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        picked.push_back(i);
+        findOne(cnt + 1, picked);
+        picked.pop_back();
+    }
+}
+
+int main(void) {
+    cin >> n >> m;
+    findOne(0, picked);
+    return 0;
+}
