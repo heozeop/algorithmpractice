7c8321eb259dcd33d7af4069098fa5ee22275c54
Lee seung chan <dltmdcks702@gmail.com>
2021-05-05 21:56:43 +0900

    add sh file to execute file on iterm

---
+++ run.sh
@@ -0,0 +1,19 @@
+if [ $# -lt 1 ] ; then
+  echo "$# is illegal number of parameters"
+  echo "Usage: $0 [file name] [?extension] [?site name]"
+  echo "ex) run.sh 11286, run.sh 11286 cpp, run.sh 11286 cpp boj"
+  exit 1
+fi
+
+current_file=$1
+current_extension=$2
+current_site=$3
+input_file=$4
+
+# current_dir=${PWD}/${current_site}
+current_dir="sites/${current_site:=boj}"
+target_file=${current_dir}/${current_file}.${current_extension:=cpp}
+
+/usr/bin/g++ -g ${target_file} -o ${current_dir}/${current_file}.out
+
+./${current_dir}/${current_file}.out < input.txt
