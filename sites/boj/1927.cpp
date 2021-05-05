04a8aba4587d1d87051720097e29c49e26a27b81
Lee seung chan <dltmdcks702@gmail.com>
2021-05-05 20:45:09 +0900

    1927.cpp

---
+++ sites/boj/1927.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+  priority_queue<long long, vector<long long>, greater<long long> > minHeap;
+
+  int n;
+  cin >> n;
+  for(int i = 0; i < n; i += 1) {
+    long long temp;
+    scanf("%ld", &temp);
+    if(temp > 0) {
+      minHeap.push(temp);
+      continue;
+    }
+    if(minHeap.empty()) {
+      printf("%ld\n", 0);
+      continue;
+    }
+
+    printf("%ld\n", minHeap.top());
+    minHeap.pop();
+  }
+
+  return 0;
+}
