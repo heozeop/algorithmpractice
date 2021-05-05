0ef816fa6579a7608299a7ee64932ac2cfcbce22
Lee seung chan <dltmdcks702@gmail.com>
2021-05-05 21:16:03 +0900

    boj 11286 cpp

---
+++ sites/boj/11286.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+typedef long long ll;
+
+ll absoluting(ll x) {
+  if(x < 0) {
+    return x * -1;
+  }
+  return x;
+}
+
+class Compare {
+  public:
+    bool operator() (ll a, ll b) {
+      ll aAbs = absoluting(a);
+      ll bAbs = absoluting(b);
+
+      if(aAbs == bAbs) {
+        return a > b;
+      }
+
+      return aAbs > bAbs;
+    }
+};
+
+int main() {
+  priority_queue<ll, vector<ll>, Compare> queue;
+
+  int n;
+  cin >> n;
+  while(n-- > 0) {
+    ll temp;
+    scanf("%lld", &temp);
+
+    if(temp != 0) {
+      queue.push(temp);
+      continue;
+    }
+
+    if(queue.empty()) {
+      printf("%d\n", 0);
+      continue;
+    }
+
+    printf("%lld\n", queue.top());
+    queue.pop();
+  }
+
+  return 0;
+}
