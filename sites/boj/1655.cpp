6859344e61b38bd3b3d8251d231da4f5c5f20926
Lee seung chan <dltmdcks702@gmail.com>
2021-05-05 23:25:09 +0900

    add boj 1655.cpp

---
+++ sites/boj/1655.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  priority_queue<int, vector<int>, less<int> > maxHeap;
+  priority_queue<int, vector<int>, greater<int> > minHeap;
+
+  int n;
+  cin >> n;
+
+  while(n-- > 0) {
+    int temp;
+    scanf("%d", &temp);
+
+    if(maxHeap.size() == 0) {
+      maxHeap.push(temp);
+    } else {
+      bool isMinTurn = maxHeap.size() > minHeap.size();
+      if(isMinTurn) {
+        minHeap.push(temp);
+      } else {
+        maxHeap.push(temp);
+      }
+
+      bool isMaxBiggerThanMin = maxHeap.top() > minHeap.top();
+      if(isMaxBiggerThanMin) {
+        temp = maxHeap.top();
+        maxHeap.pop();
+        maxHeap.push(minHeap.top());
+        minHeap.pop();
+        minHeap.push(temp);
+      }
+    }
+
+    printf("%d\n", maxHeap.top());
+  }
+  return 0;
+}
+ 
