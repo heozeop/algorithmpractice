f80554969c9f431d503ded1bf16f650465da8efb
Lee seung chan <dltmdcks702@gmail.com>
2021-12-26 21:36:48 +0900

    try(timeout): order func and visited

--- sites/boj/1753.cpp
+++ sites/boj/1753.cpp
@@ -2,16 +2,16 @@
 #include <map>
 #include <vector>
 #include <cstring>
+#include <queue>
 using namespace std;
 
 const int MAX_VERTEX_SIZE = 20001;
 const int MAX_EDGE_SIZE = 300000;
 const int INF = 11 * MAX_EDGE_SIZE;
 
-// map<pair<int,int>, int> values;
-map<int,int>values[MAX_VERTEX_SIZE];
+vector<pair<int,int> >values[MAX_VERTEX_SIZE];
 int minValues[MAX_VERTEX_SIZE];
-int visitedVertexNumber[MAX_VERTEX_SIZE];
+int visitedVertices[MAX_VERTEX_SIZE];
 int v, e;
 int startVertexNumber;
 
@@ -25,69 +25,40 @@ void init() {
     minValues[i] = INF;
   }
   minValues[startVertexNumber] = 0;
-  visitedVertexNumber[startVertexNumber] = 1;
+  visitedVertices[startVertexNumber] = 1;
 
   // 엣지 입력
   for (int i = 0; i < e; ++i) {
     int u, v, w;
     scanf("%d %d %d", &u, &v, &w);
-    if(values[u].find(v) != values[u].end() && values[u].at(v) > w) {
-      values[u].erase(v);
-    }
-    values[u].insert(make_pair(v,w));
-  }
-
-  for(auto i = values[startVertexNumber].begin(); i != values[startVertexNumber].end(); ++i) {
-    int index = i->first;
-    int value = i->second;
-
-    if(minValues[index] > value) {
-      minValues[index] = value;
-    }
-  }
-}
-
-int getNextValues() {
-  int min = INF;
-  int index = -1;
-  for (int i = 1; i <= v; ++i) {
-    if (!visitedVertexNumber[i] && minValues[i] < min) {
-      min = minValues[i];
-      index = i;
-    }
-  }
-
-  return index;
-}
-
-void updateForNext(int current) {
-  map<int,int> entryPoint = values[current];
-  for(auto iter = entryPoint.begin(); iter != entryPoint.end(); ++iter) {
-    int index = iter->first;
-    int value = iter->second;
-
-    bool isVisitedVertex = visitedVertexNumber[index] == 1;
-    if(isVisitedVertex) {
-      continue;
-    }
-
-    int throughPathWeight = minValues[current] + value;
-    if(minValues[index] > throughPathWeight) {
-      minValues[index] = throughPathWeight;
-    }
+    values[u].push_back(make_pair(v,w));
   }
 }
 
 void calcMinValues() {
-  int visitedVertex;
-  while (1) {
-    visitedVertex = getNextValues();
-    if(visitedVertex < 0) {
-      return;
-    }
+  priority_queue<pair<int, int>, vector<pair<int, int> > >minHeap;
+  minHeap.push(make_pair(0, startVertexNumber));
 
-    visitedVertexNumber[visitedVertex] = 1;
-    updateForNext(visitedVertex);
+  while (minHeap.size() > 0) {
+    int cost = -minHeap.top().first;
+    int index = minHeap.top().second;
+    visitedVertices[index] = 1;
+    minHeap.pop();
+
+    for(int i = 0; i < values[index].size(); ++i) {
+      int nextIndex = values[index][i].first;
+      int value = values[index][i].second;
+
+      if(visitedVertices[nextIndex] == 1) {
+        continue;
+      }
+
+      int throughPathWeight = cost + value;
+      if(minValues[nextIndex] > throughPathWeight) {
+        minValues[nextIndex] = throughPathWeight;
+        minHeap.push(make_pair(-throughPathWeight, nextIndex));
+      }
+    }
   }
 }
 
