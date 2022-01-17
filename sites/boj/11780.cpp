ed7e984af58a597167eff8cbafd6b7ab396bc85e
Lee seung chan <dltmdcks702@gmail.com>
2022-01-18 00:07:16 +0900

    pass: 중간에 경로는 재귀로 찾장

--- sites/boj/11780.cpp
+++ sites/boj/11780.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 const int INF = 987654321;
@@ -8,6 +9,7 @@ const int CITY_NUM_LIMIT = 100 + 1;
 int cityMap[CITY_NUM_LIMIT][CITY_NUM_LIMIT];
 int middlePath[CITY_NUM_LIMIT][CITY_NUM_LIMIT];
 int n, m;
+vector<int> path;
 
 void input();
 void solve();
@@ -24,16 +26,17 @@ void input(){
   int x, y, z;
   for(int i = 0; i < m; ++i) {
     scanf("%d %d %d", &x, &y, &z);
-    cityMap[x][y] = z;
-    cityMap[y][x] = z;
+    if(cityMap[x][y] > z) {
+      cityMap[x][y] = z;
+    }
   }
 }
 
 void floydWarshall() {
   for(int i = 1; i <= n; ++i) {
-    cityMap[i][i] = 0;
     for(int from = 1; from <= n; ++from) {
       for(int to = 1; to <= n; ++to) {
+        if(from == to) continue;
         if(cityMap[from][to] > cityMap[from][i] + cityMap[i][to]) {
           cityMap[from][to] = cityMap[from][i] + cityMap[i][to];
           middlePath[from][to] = i;
@@ -43,21 +46,42 @@ void floydWarshall() {
   }
 }
 
+void findPath(int start, int end) {
+  if(middlePath[start][end] == 0) {
+    path.push_back(start);
+    path.push_back(end);
+    return;
+  }
+  findPath(start, middlePath[start][end]);
+  path.pop_back();
+  findPath(middlePath[start][end], end);
+}
+
 void solve() {
+  floydWarshall();
   for(int i = 1; i <= n; ++i) {
     for(int j = 1; j <= n; ++j) {
-      printf("%d ", cityMap[i][j]);
+      if(cityMap[i][j] == INF) {
+        printf("0 ");
+      } else {
+        printf("%d ", cityMap[i][j]);
+      }
     }
     cout << '\n';
   }
   for(int i = 1; i <= n; ++i) {
     for(int j = 1; j <= n; ++j) {
-      if(middlePath[i][j] == 0 || i == j) {
+      if(cityMap[i][j] == INF) {
         printf("0\n");
         continue;
       }
-
-      
+      path.clear();
+      findPath(i,j);
+      printf("%d ", path.size());
+      for(int k = 0; k < path.size(); ++k) {
+        printf("%d ", path[k]);
+      }
+      printf("\n");
     }
   }
 }
