31abcafb7c8d476a4add31bd50ebfc5f46ce606c
Lee seung chan <dltmdcks702@gmail.com>
2022-01-18 21:25:55 +0900

    init

---
+++ sites/boj/2618.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+const int INF = 987654321;
+const int POINT_NUM_LIMIT = 1000 + 1;
+
+int dp[POINT_NUM_LIMIT][POINT_NUM_LIMIT];
+
+
+
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input(){
+}
+
+void solve() {
+}
