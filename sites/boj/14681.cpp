be01d0c6c512e10d7fba58192d91a3f166605bc2
Lee seung chan <dltmdcks702@gmail.com>
2022-01-18 21:34:50 +0900

    pass: 이런 날도 있는 거지

---
+++ sites/boj/14681.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include <string.h>
+
+using namespace std;
+
+int main(void) {
+  int x, y;
+  cin >> x >> y;
+  if(x < 0) {
+    if(y < 0) {
+      cout << 3;
+    } else {
+      cout << 2;
+    }
+  } else {
+    if(y < 0) {
+      cout << 4;
+    } else {
+      cout << 1;
+    }
+  }
+
+  return 0;
+}
