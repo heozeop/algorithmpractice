e83b8283613d5e485cddcdb80e69b821d19a1a88
Lee seung chan <dltmdcks702@gmail.com>
2022-01-28 22:45:39 +0900

    pass: 트리 순회
    cin.eof를 쓰기 위해서는 eof 입력 이후에 또하나의 입력을 시도해야 한다고
    합니다.

---
+++ sites/boj/5639.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+  public:
+    Node* left;
+    Node* right;
+    int val;
+
+    Node(int val) {
+      this->val = val;
+      this->left = nullptr;
+      this->right = nullptr;
+    }
+
+    void insert(int val) {
+      if(this->val < val) {
+        if(this->right == nullptr) {
+          this->right = new Node(val);
+        } else {
+          this->right->insert(val);
+        }
+      } else {
+        if(this->left == nullptr) {
+          this->left = new Node(val);
+        } else {
+          this->left->insert(val);
+        }
+      }
+    }
+};
+
+void printPostorder(Node* curNode) {
+  if(curNode == nullptr) {
+    return;
+  }
+
+  printPostorder(curNode->left);
+  printPostorder(curNode->right);
+  cout << curNode->val << endl;
+  return;
+}
+
+int main(void) {
+  int temp;
+
+  cin >> temp;
+  Node* head = new Node(temp);
+  while(cin >> temp) {
+    head->insert(temp);
+  }
+
+  printPostorder(head);
+
+  return 0;
+}
