f20d78e39f107d5f19e4a2d59519349f370780ab
Lee seung chan <dltmdcks702@gmail.com>
2022-02-09 21:30:44 +0900

    pass: boj 1976 find path using union find

--- sites/boj/1976.cpp
+++ sites/boj/1976.cpp
@@ -5,8 +5,16 @@
 
 using namespace std;
 
+const int LIMIT_NODE_COUNT = 201;
+
 void input();
 void solve();
+int find();
+void merging(int, int);
+
+int n, m;
+int arr[LIMIT_NODE_COUNT];
+vector<int> target;
 
 int main(void) {
   input();
@@ -15,9 +23,56 @@ int main(void) {
 }
 
 void input() {
+  cin >> n >> m;
+  for(int i = 1; i <= n; ++i) {
+    arr[i] = i;
+  }
+
+  int connection = 0;
+  for(int i = 1; i <= n; ++i) {
+    for(int j = 1; j <= n; ++j) {
+      cin >> connection;
+      if(connection) {
+        merging(i,j);
+      }
+    }
+  }
+
+  int temp = 0;
+  for(int i = 0; i <m; ++i) {
+    cin >> temp;
+    target.push_back(temp);
+  }
   return;
 }
 
+int find(int a) {
+  if(a == arr[a]) {
+    return a;
+  }
+
+  return arr[a] = find(arr[a]);
+}
+
+void merging(int a, int b) {
+  int aP = find(a);
+  int bP = find(b);
+
+  arr[aP] = arr[bP];
+}
+
 void solve() {
+  int a, b;
+
+  for(int i = 1; i < target.size(); ++i) {
+    a = target[i - 1];
+    b = target[i];
+
+    if(find(a) != find(b)) {
+      cout << "NO";
+      return;
+    }
+  }
+  cout << "YES";
   return;
 }
