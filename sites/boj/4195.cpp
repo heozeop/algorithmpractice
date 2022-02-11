447b0364acba2652e84b28b563044f5f81a4a077
Lee seung chan <dltmdcks702@gmail.com>
2022-02-11 19:05:18 +0900

    pass: boj 4195 친구 네트워크
    union find
    사이클이 없을 것이라고 섯불리 단정지었다.

---
+++ sites/boj/4195.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+const int LIMIT_NODE_COUNT = 200001;
+
+void input();
+void init();
+void solve();
+
+int n, f;
+
+int connected[LIMIT_NODE_COUNT];
+int nodes[LIMIT_NODE_COUNT];
+
+int main(void) {
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> n;
+  return;
+}
+
+void init() {
+  for(int i = 0; i < LIMIT_NODE_COUNT; ++i) {
+    nodes[i] = i;
+    connected[i] = 1;
+  }
+}
+
+int find(int a) {
+  if(a == nodes[a]) {
+    return a;
+  }
+  
+  return nodes[a] = find(nodes[a]);
+}
+
+int merge(int a, int b) {
+  int aP = find(a);
+  int bP = find(b);
+
+
+  int big, small;
+  if (connected[aP] < connected[bP]) {
+    big = bP;
+    small = aP;
+  } else {
+    big = aP;
+    small = bP;
+  }
+
+  if(aP == bP) {
+    return connected[big];
+  }
+
+  connected[big] += connected[small];
+  nodes[small] = big;
+
+  return connected[big];
+}
+
+void solve() {
+  string a,b;
+  while(n--) {
+    cin >> f;
+
+    map<string, int> nameIdMap;
+    int friendId = 1;
+
+    init();
+
+    while(f--) {
+      cin >> a >> b;
+
+      if(nameIdMap.count(a) == 0) {
+        nameIdMap[a] = friendId++;
+      }
+
+      if(nameIdMap.count(b) == 0) {
+        nameIdMap[b] = friendId++;
+      }
+
+      cout << merge(nameIdMap[a], nameIdMap[b]) << '\n';
+    }
+  }
+  return;
+}
