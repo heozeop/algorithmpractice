1efb6f10d6c4172080c172a0b65a7f3d49688213
Lee seung chan <dltmdcks702@gmail.com>
2022-02-13 21:21:52 +0900

    pass: boj 9372 mst?

---
+++ sites/boj/9372.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+void input();
+void solve();
+
+int n,m,t;
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+
+void input() {
+  cin >> t;
+  return;
+}
+
+void solve() {
+  int a,b;
+  while(t--) {
+    cin >> n >> m;
+    while(m--) {
+      cin >> a >> b;
+    }
+
+    cout << n - 1 << '\n';
+  }
+  return;
+}
