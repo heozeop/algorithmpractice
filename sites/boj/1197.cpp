a35fb48181f223494a5cc2c4540fed92a3496fbc
Lee seung chan <dltmdcks702@gmail.com>
2022-02-15 21:37:40 +0900

    pass: boj 1197 mst
    최소 힙으로 간선을 정하고, 유니온 파인드로 방문 여부를 검증했습니다.
    수학적으로 MST를 증명하는 방법을 알아봐야겠습니다.

---
+++ sites/boj/1197.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+typedef pair<int,pair<int,int> > piii;
+const int LIMIT_NODE_COUNT = 10001;
+
+void input();
+void solve();
+
+int v, e;
+priority_queue<piii, vector<piii>, greater<piii> >graph;
+int minSpanningTree;
+int list[LIMIT_NODE_COUNT];
+int length[LIMIT_NODE_COUNT];
+int cost[LIMIT_NODE_COUNT];
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> v >> e;
+  int a,b,c;
+  while(e--) {
+    cin >> a >> b >> c;
+    graph.push(make_pair(c, make_pair(a,b)));
+  }
+
+  for(int i = 1; i <= v; ++i) {
+    list[i] = i;
+  }
+  return;
+}
+
+int findParent(int a) {
+  if(list[a] == a) {
+    return a;
+  }
+
+  return list[a] = findParent(list[a]);
+}
+
+int merge(int a, int b) {
+  int aP = findParent(a);
+  int bP = findParent(b);
+  if(aP != bP) {
+    list[aP] = bP;
+    length[aP] += length[bP];
+    return length[aP];
+  }
+  return -1;
+}
+
+void solve() {
+  piii temp;
+  int result;
+
+  while(!graph.empty() || result >= v) {
+    temp = graph.top();
+    graph.pop();
+    result = merge(temp.second.first, temp.second.second);
+    if(result != -1) {
+      minSpanningTree += temp.first;
+    }
+  }
+
+  cout << minSpanningTree << endl;
+
+  return;
+}
