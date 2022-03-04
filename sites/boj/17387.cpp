890950091536262583db2106c4b4214e50e86dda
Lee seung chan <dltmdcks702@gmail.com>
2022-03-04 22:14:22 +0900

    pass: boj 17387 ccw
    답지를 봐버려서 0일 경우에 대한 아이디어를 따로 생각해 낸게 아니다.
    보는 건 좋은데, 생각을 할 수 있도록 조심해야겠다.

---
+++ sites/boj/17387.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+void input();
+void solve();
+
+pll p[4];
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  for(int i = 0; i < 4; ++i) {
+    cin >> p[i].first >> p[i].second;
+  }
+  return;
+}
+
+int ccw(pll a, pll b, pll c) {
+  ll pos = a.first * b.second + b.first * c.second + c.first * a.second;
+  ll neg = c.first * b.second + b.first * a.second + a.first * c.second;
+
+  ll result = pos - neg;
+
+  if(result == 0) {
+    return 0;
+  }
+
+  if(result > 0) {
+    return 1;
+  }
+
+  return -1;
+}
+
+bool isCbetweenAB(pll a, pll b, pll c) {
+  if(a.first == c.first && a.second == c.second) {
+    return 1;
+  }
+
+  if(b.first == c.first && b.second == c.second) {
+    return 1;
+  }
+
+  if(a.first <= c.first) {
+    if(a.second <= c.second) {
+      return c.first <= b.first && c.second <= b.second;
+    } else {
+      return c.first <= b.first && c.second >= b.second;
+    }
+  } else {
+    if(a.second <= c.second) {
+      return c.first >= b.first && c.second <= b.second;
+    } else {
+      return c.first >= b.first && c.second >= b.second;
+    }
+  }
+}
+
+void solve() {
+  int a = ccw(p[0], p[1], p[2]), 
+    b = ccw(p[0], p[1], p[3]),
+    c = ccw(p[2], p[3], p[0]), 
+    d = ccw(p[2], p[3], p[1]);
+
+  if(a * b == 0 && c * d == 0) {
+    cout << (isCbetweenAB(p[0], p[1], p[2]) || isCbetweenAB(p[0], p[1], p[3]) || isCbetweenAB(p[2],p[3],p[0]) || isCbetweenAB(p[2], p[3], p[1]));
+  } else if(a * b <= 0 && c * d <= 0) {
+    cout << 1;
+  } else {
+    cout << 0;
+  }
+
+  return;
+}
