40ba9f1049009d82d7a78e467bb558ba3ea7f0cb
Lee seung chan <dltmdcks702@gmail.com>
2022-03-08 22:07:50 +0900

    pass: boj 1069

---
+++ sites/boj/1069.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+void input();
+void solve();
+
+int x,y,d,t;
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> x >> y >> d >> t;
+  return;
+}
+
+void solve() {
+  double distance = sqrt(x * x + y * y);
+  double ans = distance;
+
+  int jumpCount = distance / d;
+  double left = distance - jumpCount * d;
+  ans = min(ans, left + jumpCount * t);
+  ans = min(ans, d - left + (jumpCount + 1) * t);
+  if(distance < d) {
+    ans = min(ans, 2.0 * t);
+  } else {
+    ans = min(ans, (double)(jumpCount + 1) * t);
+  }
+
+  printf("%.10lf", ans);
+  return;
+}
