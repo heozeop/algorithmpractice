132806781a2a3429f7dfa3f6c2b1b4e2306bda1f
Lee seung chan <dltmdcks702@gmail.com>
2022-03-09 18:58:35 +0900

    pass: boj 11723 bitset stl 알기

---
+++ sites/boj/11723.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <string>
+#include <bitset>
+
+using namespace std;
+
+void input();
+void solve();
+
+int n;
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> n;
+  return;
+}
+
+void solve() {
+  bitset<21> bs;
+  string op;
+  int val;
+  for(int i = 0; i < n; i++) {
+    cin >> op;
+    if(op == "all") {
+      bs.set();
+    } else if(op == "empty") {
+      bs.reset();
+    } else {
+      cin >> val;
+      if(op == "add") {
+        bs.set(val);
+      }
+      if(op == "remove") {
+        bs.reset(val);
+      }
+      if(op == "check") {
+        cout << bs.test(val) << '\n';
+      }
+      if(op == "toggle") {
+        bs.flip(val);
+      }
+    }
+  }
+  return;
+}
