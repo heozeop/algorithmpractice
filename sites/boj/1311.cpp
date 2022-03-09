e8a143550679081c18d8385241b045ceeeb87ba0
Lee seung chan <dltmdcks702@gmail.com>
2022-03-09 20:48:16 +0900

    pass: boj 1311 dp
    방향성만 생각해서 같은 작업을 다른 조건에서 다시 할거라는 생각을 못한 것
    같다. 굉장히 아쉽다.

---
+++ sites/boj/1311.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+const int INF = 123456789;
+
+void input();
+void solve();
+
+int n;
+int D[21][21];
+int dp[20][1 << 21];
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> n;
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++) {
+      cin >> D[i][j];
+    }
+  }
+  return;
+}
+
+int find(int pn, unsigned int workLeft) {
+  if(pn >= n || workLeft == ((1 << n) - 1)) return 0;
+  if(dp[pn][workLeft]) return dp[pn][workLeft];
+
+  int val = INF;
+  unsigned int nextLeft;
+
+  for(int i = 0; i < n; i++) {
+    if(workLeft & (1 << i)) continue;
+
+    nextLeft = workLeft + (1 << i);
+
+    val = min(val, find(pn + 1, nextLeft) + D[pn][i]);
+  }
+
+  if(!dp[pn][workLeft] || dp[pn][workLeft] > val) {
+    dp[pn][workLeft] = val;
+  }
+
+  return dp[pn][workLeft];
+}
+
+void solve() {
+  cout << find(0, 0);
+  return;
+}
