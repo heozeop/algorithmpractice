0151be02c473eabb806e4443852fadecef24ad44
Lee seung chan <dltmdcks702@gmail.com>
2022-03-12 14:52:17 +0900

    pass: 2225 dp
    점화식 없이 풀었다.
    무식하게 푸니, 시간이 오래걸린다. 점화식을 세우고, 구현가능하게
    해봐야겠다.

---
+++ sites/boj/2225.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+const int MOD = 1000000000;
+
+void input();
+void solve();
+
+int n, k;
+long long dp[201][201];
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> n >> k;
+  return;
+}
+
+void solve() {
+  for(int i = 1; i <= k; i++) {
+    dp[0][i] = 1;
+  }
+  for(int i = 1; i <= n; i++) {
+    for(int j = 1; j <= k; j++) {
+      dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
+    }
+  }
+  cout << dp[n][k];
+  return;
+}
