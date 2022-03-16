85e0b7e71ed6e43cf3fcba664572a3b6086d47c7
Lee seung chan <dltmdcks702@gmail.com>
2022-03-16 12:09:08 +0900

    pass: boj 4354
    failure function을 도구로써 이용한다는 느낌으로 문제를 풀면
    되었습니다만, 답을 보고 안거라서 더 많이 관련 문제를 풀고 익혀야 할 것
    같습니다. 아직 갈길이 멉니다. ㅜㅜ

--- sites/boj/4354.cpp
+++ sites/boj/4354.cpp
@@ -9,7 +9,6 @@ using namespace std;
 void input();
 void solve();
 
-int t;
 string s;
 
 int main(void) {
@@ -22,8 +21,34 @@ void input() {
   return;
 }
 
+vector<int> failureFunction(string s) {
+  int j = 0;
+  vector<int> pi(s.length(), 0);
+  for(int i = 1; i < s.length(); i++) {
+    while(j > 0 && s[i] != s[j]) j = pi[j - 1];
 
+    if(s[i] == s[j]) {
+      pi[i] = ++j;
+    }
+  }
+  return pi;
+}
 
 void solve() {
+  while(1) {
+    getline(cin, s);
+    if(s == ".") {
+      break;
+    }
+    auto pi = failureFunction(s);
+    // 구하고자 하는게 s에서 반복되는 것을 찾는 것이라 마지막 것만 중요
+    int length = s.length();
+    int last = pi[length - 1];
+    if(last == 0 || length % (length - last) != 0) {
+      cout << 1 << '\n';
+    } else {
+      cout << length / (length - last) << '\n';
+    }
+  }
   return;
 }
