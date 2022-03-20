a4ab27d961ec7e22fce430c5f5f47fc2c8ce3912
Lee seung chan <dltmdcks702@gmail.com>
2022-03-20 20:03:34 +0900

    pass: boj 14425

---
+++ sites/boj/14425.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Node {
+private:
+  map<char, Node*> children;
+  bool isLastNode = false;
+public:
+  void insert(string& nodes, int index = 0) {
+    if (index == nodes.size()) {
+      isLastNode = true;
+      return;
+    }
+
+    if (children.find(nodes[index]) == children.end()) {
+      children[nodes[index]] = new Node();
+    }
+    children[nodes[index]]->insert(nodes, index + 1);
+  }
+
+  bool find(string& str, int index = 0) {
+    if(index == str.size()) return isLastNode;
+
+    if(children.find(str[index]) != children.end()) {
+      return children[str[index]]->find(str, index + 1);
+    }
+    return false;
+  }
+};
+
+void input();
+void solve();
+
+int n, m;
+Node* root;
+vector<string> finds;
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  root = new Node();
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> n >> m;
+  for(int i = 0; i < n; i++) {
+    string a;
+    cin >> a;
+    root->insert(a);
+  }
+  for(int i = 0; i < m; i++) {
+    string a;
+    cin >> a;
+    finds.push_back(a);
+  }
+  return;
+}
+
+void solve() {
+  int found = 0;
+  for(int i = 0; i < m; i++) {
+    if(root->find(finds[i])) found++;
+  }
+  cout << found;
+  return;
+}
