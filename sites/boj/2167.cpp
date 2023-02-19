9b237b99f0350f3071b647f3a958ac4a652adeda
Lee seung chan <dltmdcks702@gmail.com>
2023-02-20 07:50:24 +0900

    solve: boj silver 5 2167
    10:04
    - 함수 모듈화
        - 단계별로 생각하고 작업 하기 연습 계속 필요

---
+++ sites/boj/2167.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <limits.h>
+
+using namespace std;
+
+void input();
+void solve();
+int n, m, k;
+int array2D[301][301];
+int calculateSum();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> n >> m;
+  for(int i = 1; i <= n; ++i) {
+    for(int j = 1; j <= m; ++j) {
+      cin >> array2D[i][j];
+    }
+  }
+  cin >> k;
+  return;
+}
+
+void solve() {
+  for(int i = 0 ; i < k; ++i) {
+    cout << calculateSum() << '\n';
+  }
+
+  return;
+}
+
+int calculateSum() {
+  int i,j,x,y;
+  int sum = 0;
+
+  cin >> i >> j >> x >> y;
+  for(int row = i; row <= x; ++row) {
+    for(int col = j; col <= y; ++col) {
+      sum += array2D[row][col];
+    }
+  }
+
+  return sum;
+}
