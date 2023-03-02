e41d56d0fc71ca545991849caa14e34690158223
Lee seung chan <dltmdcks702@gmail.com>
2023-03-02 23:43:18 +0900

    solve: boj gold 4 14500
    주어진 조건으로 돌리다보면 4칸 이동해서 구할 수 있는 조합이 나옵니다.
    이를 이용해서 위치마다 4칸 돌려보고 최대 값을 찾으면 됩니다.
    단, ㅓ/ㅏ/ㅗ/ㅜ는 2중 for로 확인이 불가하니 직접 합니다.

--- sites/boj/14500.cpp
+++ sites/boj/14500.cpp
@@ -6,80 +6,20 @@
 
 using namespace std;
 const int MAX_SIZE = 500;
-const int NUM_OF_TYPE = 19;
+const int DIRECTIONS[4][2] = {
+  {-1,0},
+  {0,1},
+  {1,0},
+  {0, -1},
+};
 
 int paperMap[MAX_SIZE][MAX_SIZE];
+int paperVisitMap[MAX_SIZE][MAX_SIZE];
 int n, m;
-
-class Tetromino {
-  int position[4][2];
-public:
-  Tetromino() {}
-  Tetromino(int position[4][2]) { 
-    this->setPosition(position);
-  }
-
-  void setPosition(int position[4][2]) {
-    for(int i = 0; i < 4; ++i) {
-      this->position[i][0] = position[i][0];
-      this->position[i][1] = position[i][1];
-    }
-  }
-
-  bool isPossible(int i, int j) {
-    int ni,nj;
-    for(int k = 0; k < 4; ++k) {
-      ni = i + this->position[k][0];
-      nj = j + this->position[k][1];
-
-      if(ni >= n || nj >= m) {
-        return false;
-      }
-    }
-
-    return true;
-  };
-
-  int sum(int i, int j) {
-    int localMax = 0;
-    int ni,nj;
-
-    for(int k = 0; k < 4; ++k) {
-      ni = i + this->position[k][0];
-      nj = j + this->position[k][1];
-
-      localMax += paperMap[ni][nj];
-    }
-
-    return localMax;
-  }
-};
-
-int typeList[NUM_OF_TYPE][4][2] = {
-  {{0,0},{0,1},{0,2}, {0, 3}},
-  {{0,0},{1,0},{2,0},{3, 0}},
-  {{0,0},{0,1},{1,0},{1, 1}},
-  {{0,0},{1,0},{2,0},{2, 1}},
-  {{0,0},{0,1},{0,2},{1, 0}},
-  {{0,0},{0,1},{1,1},{2, 1}},
-  {{0,0},{0,1},{0,2},{-1, 2}},
-  {{0,0},{1,0},{1,1},{2, 1}},
-  {{0,0},{0,1},{-1,1},{-1, 2}},
-  {{0,0},{0,1},{0,2},{1, 1}},
-  {{0,0},{0,1},{1,1},{-1, 1}},
-  {{0,0},{1,0},{2,0},{1, 1}},
-  {{0,0},{0,1},{0,2},{-1, 1}},
-  {{0,0},{0,1},{-1,1},{-2, 1}},
-  {{0,0},{1,0},{1,1},{1, 2}},
-  {{0,0},{0,1},{1,0},{2, 0}},
-  {{0,0},{0,1},{0,2},{1, 2}},
-  {{0,0},{1,0},{0,1},{-1, 1}},
-  {{0,0},{0,1},{1,1},{1, 2}},
-};
-
 
 void input();
 void solve();
+int getGlobalMax(int i, int j, int count, int curMax);
 
 int main(void) {
   input();
@@ -99,23 +39,50 @@ void input() {
 }
 
 void solve() {
-  Tetromino curTetromino;
-  int globalMax, localMax;
-  globalMax = localMax = 0;
-  for(int k = 0; k < NUM_OF_TYPE; ++k) {
-    curTetromino.setPosition(typeList[k]);
-    localMax = 0;
-    for(int i = 0; i < n; ++i) {
-      for(int j = 0; j < m; ++j) {
-        if(curTetromino.isPossible(i,j)) {
-          localMax = max(localMax, curTetromino.sum(i,j));
-        }
-      }
+  int globalMax = 0;
+  for(int i = 0; i < n; ++i) {
+    for(int j = 0; j < m; ++j) {
+      paperVisitMap[i][j] = true;
+      globalMax = max(globalMax, getGlobalMax(i, j, 1, paperMap[i][j]));
+      paperVisitMap[i][j] = false;
     }
-
-    globalMax = max(globalMax, localMax);
   }
 
   cout << globalMax << endl;
   return;
+}
+
+int getGlobalMax(int x, int y, int count, int curMax) {
+  if(count >= 4) {
+    return curMax;
+  }
+
+  int nx,ny;
+  int localMax = curMax;
+  for(int i = 0; i < 4; ++i) {
+    nx = x + DIRECTIONS[i][0];
+    ny = y + DIRECTIONS[i][1];
+    if( nx < 0 || ny < 0|| nx >= n || ny >= m || paperVisitMap[nx][ny]) {
+      continue;
+    }
+
+    paperVisitMap[nx][ny] = true;
+    localMax = max(localMax, getGlobalMax(nx,ny,count + 1, curMax + paperMap[nx][ny]));
+    paperVisitMap[nx][ny] = false;
+  }
+
+  if(x-1 >= 0 && y-1 >= 0 && x+1 < n) { //ㅓ
+      localMax = max(localMax, (paperMap[x-1][y] + paperMap[x][y-1] + paperMap[x][y] + paperMap[x+1][y]));
+  }
+  if(x-1 >= 0 && y+1 < m && x+1 < n) { //ㅏ
+      localMax = max(localMax, (paperMap[x-1][y] + paperMap[x][y+1] + paperMap[x][y] + paperMap[x+1][y]));
+  }
+  if(y-1 >= 0 && y+1 < m && x+1 < n) { //ㅗ
+      localMax = max(localMax, (paperMap[x][y] + paperMap[x+1][y] + paperMap[x+1][y-1] + paperMap[x+1][y+1]));
+  }
+  if(y-1 >= 0 && y+1 < m && x+1 < n) { //ㅜ
+      localMax = max(localMax, (paperMap[x][y-1] + paperMap[x][y] + paperMap[x][y+1] + paperMap[x+1][y]));
+  }
+
+  return localMax;
 }
