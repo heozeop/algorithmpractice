f5acdb05df6c3e308b754c6dc3caa8fcb045865e
Lee seung chan <dltmdcks702@gmail.com>
2023-03-13 20:27:18 +0900

    solve: boj gold 4 14499
    - 평가
        - 각 단계별로 생각하는데 집중하자. 전체적인 구조를 유지하면서 단계를
          함께 생각하면 복잡해 진다.
    1시간 47분
    - 첫 구현: 1시간 08초
        - dice의 표현과 회전을 동시에 생각했던 것 같다. 그래서 회전 하는
          축을 중심으로 4개 array를 만들어 돌려주려고 했다. 하지만 그게
          제대로 작동하는 확신이 없었고, 결국 찾아보게 되었다.
    - 실수
        - dice를 6개 array로 표현 후 변환하는 부분에서 방향 변수 등의 실수가
          있었다. direction 만들때 항상 0부터 시작한다는 부분을 명심하자.

---
+++ sites/boj/14499.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <limits.h>
+
+using namespace std;
+const int EAST = 0;
+const int WEST = 1;
+const int NORTH = 2;
+const int SOUTH = 3;
+const int DIRECTION[4][2] = {
+  {0, 1},
+  {0, -1},
+  {-1, 0},
+  {1, 0}
+};
+typedef pair<int,int> pos;
+
+void input();
+void solve();
+
+int board[20][20];
+int opList[1000];
+int dice[6];
+int top = 2, bottom = 4;
+
+bool isMovable(pos);
+pos getNextPos(int);
+void move(int);
+void copyBottom();
+void printTop();
+
+int n, m, curX, curY, k;
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> n >> m >> curX >> curY >> k;
+
+  for(int i = 0; i < n; ++i) {
+    for(int j = 0; j < m; ++j) {
+      cin >> board[i][j];
+    }
+  }
+
+  for(int i = 0; i < k; ++i) {
+    cin >> opList[i];
+  }
+
+  return;
+}
+
+void solve() {
+  int direction = 0;
+  pos nextPos;
+  for (int i = 0; i < k; ++i) {
+    direction = opList[i] - 1;
+    nextPos = getNextPos(direction);
+    if(!isMovable(nextPos)) {
+      continue;
+    }
+
+    move(direction);
+    copyBottom();
+    printTop();
+  }
+
+  return;
+}
+
+pos getNextPos(int direction) {
+  return {
+    curX + DIRECTION[direction][0],
+    curY + DIRECTION[direction][1],
+  };
+}
+
+bool isMovable(pos nextPos) {
+  int x = nextPos.first;
+  int y = nextPos.second;
+
+  if(x < 0 || y < 0 || x >= n || y >= m) {
+    return false;
+  }
+
+  return true;
+}
+
+void move(int direction) {
+  curX += DIRECTION[direction][0];
+  curY += DIRECTION[direction][1];
+
+  vector<int> temp(dice, dice + 6);
+  switch(direction) {
+    case EAST:
+      dice[0] = temp[2];
+      dice[1] = temp[4];
+      dice[2] = temp[1];
+      dice[4] = temp[0];
+      break;
+    case WEST:
+      dice[0] = temp[4];
+      dice[1] = temp[2];
+      dice[2] = temp[0];
+      dice[4] = temp[1];
+      break;
+    case NORTH:
+      dice[2] = temp[5];
+      dice[3] = temp[2];
+      dice[4] = temp[3];
+      dice[5] = temp[4];
+      break;
+    case SOUTH:
+      dice[2] = temp[3];
+      dice[3] = temp[4];
+      dice[4] = temp[5];
+      dice[5] = temp[2];
+      break;
+  }
+}
+
+void copyBottom() {
+  if(board[curX][curY] == 0) {
+    board[curX][curY] = dice[bottom];
+  } else {
+    dice[bottom] = board[curX][curY];
+    board[curX][curY] = 0;
+  }
+}
+
+void printTop() {
+  cout << dice[top] << endl;
+}
