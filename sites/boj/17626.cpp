85e729eed1c43be7564625ac6952db8db9eac8ea
Lee seung chan <dltmdcks702@gmail.com>
2023-03-17 19:30:12 +0900

    solve: boj silve 3 17626
    만족스럽지 못하다.
    뭔가 계산이 많다. 흐음... 불편하구먼
    
    38분 걸린 것도 아쉽다. 문제 파악에 시간을 많이 쓰고 있다. 음 일단 너무
    조급하지 말자. 익숙해 지는 것도 일이다.

---
+++ sites/boj/17626.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <limits.h>
+#include <math.h>
+
+using namespace std;
+int n;
+int dp[50001];
+
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> n;
+  return;
+}
+
+void solve() {
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 3;
+  dp[4] = 1;
+
+  float temp, flooredTemp;
+  for(int i = 5; i <= n; ++i) {
+    temp = sqrt(i);
+    flooredTemp = floor(temp);
+    if(flooredTemp == temp) {
+      dp[i] = 1;
+      continue;
+    } 
+
+    dp[i] = 5;
+    for(int j = flooredTemp; j > 0 && dp[i] > 2; --j) {
+      dp[i] = min(1 + dp[i - (int)pow(j, 2)], dp[i]);
+    }
+  }
+  cout << dp[n];
+
+  return;
+}
