41bb0b3a9512d47cf1cc84653baf4354122f851c
Lee seung chan <dltmdcks702@gmail.com>
2023-03-22 22:33:49 +0900

    solve: boj gold4 2011
    52분
    예외 상황을 찾아보고 나서야 제대로 찾았다.
    내가 대충 대충 넘기려는 성향이 있는 것 같다.
    안되는 상황을 생각한 뒤, 하나 찾고 나서 조건 식으로 생각하지 않고 빠르게
    논리 전개를 이어간 것 같다. 계속 조건을 생각하면서 문제를 풀어나가보자.

---
+++ sites/boj/2011.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <limits.h>
+
+using namespace std;
+
+void input();
+void solve();
+
+int n;
+int sc[5001];
+int dp[5001];
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  string temp;
+  cin >> temp;
+  n = temp.length();
+  int i = 1;
+  for(char c : temp) {
+    sc[i++] = c - '0';
+  }
+
+  return;
+}
+
+void solve() {
+  if(sc[1] == 0) {
+    cout << 0;
+    return;
+  }
+  dp[0] = 1;
+  dp[1] = 1;
+
+  int temp;
+  for(int i=2; i <= n; ++i) {
+    if(sc[i] == 0 && (sc[i-1] == 0 || sc[i-1] > 2)) {
+      cout << 0;
+      return;
+    }
+
+    if (sc[i] > 0) {
+      dp[i] += dp[i - 1];
+    }
+
+    temp = sc[i] + sc[i - 1] * 10;
+
+    if(temp >= 10 && temp <= 26) {
+      dp[i] += dp[i - 2];
+    } 
+
+    dp[i] %= 1000000;
+  }
+  cout << dp[n];
+
+  return;
+}
