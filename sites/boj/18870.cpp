8257bd06c53cf8e363ea138727e514945072414e
Lee seung chan <dltmdcks702@gmail.com>
2023-05-14 12:05:01 +0900

    solve: boj silver 2 18870
    그냥 자료구조 아는지 물어보는 문제
    1. 30분 걸림

---
+++ sites/boj/18870.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <limits.h>
+#include <map>
+
+using namespace std;
+
+const int MAX_N = 1000000;
+
+int n;
+int arr[MAX_N];
+
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> n;
+
+  for(int i = 0; i < n; ++i) {
+    cin >> arr[i];
+  }
+
+  return;
+}
+
+void solve() {
+  map<int, int> m;
+  vector<int> a(arr, arr+n);
+
+  sort(a.begin(), a.end());
+
+  for(int i = 0, j = 0; i < n; ++i) {
+    if(m.find(a[i]) == m.end()) {
+      m[a[i]] = j++;
+    }
+  }
+
+  map<int,int>::iterator iter;
+  for(int i = 0; i < n;++i) {
+    iter = m.find(arr[i]);
+    cout << iter->second << " ";
+  }
+
+  return;
+}
