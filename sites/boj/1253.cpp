130876d24a321d0674f639469c38a2c07e91e34b
Lee seung chan <dltmdcks702@gmail.com>
2023-05-27 07:45:33 +0900

    solve: boj gold 4 1253
    1. 기본 로직
        1. 2수의 합으로 set생성
        1. 비교
    1. 유의점
        1. 자기자신과 같은 수 가능
        1. 자기 자신을 제외한 숫자의 조합에서 찾아야 함
    1. 접근
        1. 자기자신과 같은 값 개수 세기
        1. 후보군과 같은 개수 세서 같은 값 개수가 같은 개수 초과인지 확인

---
+++ sites/boj/1253.cpp
@@ -0,0 +1,91 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int n;
+int arr[2000];
+
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> n;
+  for (int i = 0; i < n; ++i) {
+    cin >> arr[i];
+  }
+  return;
+}
+
+void solve() {
+  if (n < 2) {
+    cout << 0;
+    return;
+  }
+
+  map<int,int> arrMap;
+  for(int i = 0; i < n; ++i) {
+    if(arrMap.find(arr[i]) == arrMap.end()) {
+      arrMap.insert({arr[i], 1});
+    } else {
+      arrMap[arr[i]] += 1;
+    }
+  }
+
+  set<int> goodCandidates;
+
+  int candidate, sameCount;
+  for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; ++j) {
+      candidate = arr[i] + arr[j];
+
+      sameCount = 0;
+      if (candidate == arr[i]) {
+        sameCount += 1;
+      }
+
+      if(candidate == arr[j]) {
+        sameCount += 1;
+      }
+
+      // 이거는 과하다. 0.0.0 막는건데 1개만 있어도 된다.
+      if (arrMap.find(candidate)->second <= sameCount) {
+        continue;
+      }
+
+      auto iter = goodCandidates.find(candidate);
+
+      if (iter != goodCandidates.end()) {
+        continue;
+      }
+
+      goodCandidates.insert(candidate);
+    }
+  }
+
+  int numberOfGoodNumbers = 0;
+  for (int i = 0; i < n; ++i) {
+    auto iter = goodCandidates.find(arr[i]);
+
+    if (iter == goodCandidates.end()) {
+      continue;
+    }
+
+    numberOfGoodNumbers += 1;
+  }
+
+  cout << numberOfGoodNumbers;
+
+  return;
+}
