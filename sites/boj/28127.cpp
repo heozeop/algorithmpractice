dbb14203a2ee8a89fca240c7901cf063a545940a
Lint Action <lint-action@samuelmeuli.com>
2023-05-27 20:39:18 +0000

    Fix code style issues with clang_format

---
+++ sites/boj/28127.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int a, d, x, q;
+
+void input();
+void solve();
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> q;
+  return; 
+}
+
+void calc() {
+  int cur = a;
+  int row = 1;
+
+  while(cur < x) {
+    cur += a + row * d;
+    row += 1;
+  }
+
+  int col = cur - a - (row - 1) * d;
+  cout << row << " " << x - col << "\n";
+}
+
+
+void solve() { 
+  while(q--) {
+    cin >> a >> d >> x;
+    calc();
+  }
+
+  return; 
+}
+
+
