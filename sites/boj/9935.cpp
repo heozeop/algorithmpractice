ae83d53cd669065f96336e077f2101b313403ad0
Lee seung chan <dltmdcks702@gmail.com>
2023-06-02 18:28:11 +0900

    copy: boj gold 4 9935
    1. 비교하는 범위 조정하기

---
+++ sites/boj/9935.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iterator>
+
+using namespace std;
+
+void input();
+void solve();
+
+string inputValue;
+string explosive;
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> inputValue >> explosive;
+  return; 
+}
+
+string exploadAll(string inputString) {
+  if(inputString.length() < explosive.length()) {
+    return inputString;
+  }
+
+  string temp = "";
+  for(int i = 0; i < inputString.length(); ++i) {
+    temp += inputString[i];
+    if (temp.length() < explosive.length()) {
+      continue;
+    }
+
+    bool isExplosiveExist = true;
+    for(int j = 0; j < explosive.length(); ++j) {
+      if (temp[temp.length() - explosive.length() + j] != explosive[j]) {
+        isExplosiveExist = false;
+        break;
+      }
+    }
+
+    if (isExplosiveExist) {
+      temp.erase(temp.end() - explosive.length(), temp.end());
+    }
+  }
+
+  return temp;
+}
+
+void solve() { 
+  string targetString = exploadAll(inputValue);
+
+  if(targetString.length() > 0) {
+    cout << targetString;
+    return;
+  }
+
+  cout << "FRULA";
+
+
+
+  return; 
+}
