092643dc1d3913f3af4252e32785d7a4bccbb1eb
Lee seung chan <dltmdcks702@gmail.com>
2023-06-04 04:41:13 +0900

    fix: boj gold 3 10986
    1. count의 크기가 n^2이기 때문에 int 범위가 벗어난다.

--- sites/boj/10986.cpp
+++ sites/boj/10986.cpp
@@ -11,7 +11,7 @@ const int MAX_M = 1000;
 
 int n, m;
 int arr[MAX_N + 1];
-int cnt[MAX_M + 1];
+long long cnt[MAX_M + 1];
 
 void input();
 void solve();
@@ -33,21 +33,19 @@ void input() {
 
 void solve() {
 
-  // init sum
-  int csum = 0;
-  for (int i = 1; i <= n; ++i) {
+  long long csum = 0;
+  for(int i = 1; i <= n; ++i) {
     csum += arr[i];
 
-    cnt[csum % m] += 1;
+    cnt[(csum % (long long)m)] += 1;
   }
 
-  // travel with two pointer
-  int mcount = 0;
-  for (int i = 0; i <= MAX_M; ++i) {
+  long long mcount = 0;
+  for(int i = 0; i < m; ++i) {
     mcount += cnt[i] * (cnt[i] - 1) / 2;
   }
-
-  cout << mcount + cnt[0]; // 조합 + 일반
+    
+  cout << mcount + cnt[0]; // 조합 + 1개
 
   return;
 }
