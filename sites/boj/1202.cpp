35826509cd4dde5a3810c47fe78fb1c29461ed7b
Lint Action <lint-action@samuelmeuli.com>
2023-06-05 11:43:55 +0000

    Fix code style issues with clang_format

--- sites/boj/1202.cpp
+++ sites/boj/1202.cpp
@@ -10,8 +10,8 @@ typedef long long ll;
 const int MAX_NUM_OF_BAG = 300000;
 const int MAX_NUM_OF_GEM = 300000;
 
-int n,k;
-pair<int,int> gemstones[MAX_NUM_OF_GEM];
+int n, k;
+pair<int, int> gemstones[MAX_NUM_OF_GEM];
 int bagArr[MAX_NUM_OF_BAG];
 
 void input();
@@ -23,34 +23,34 @@ int main(void) {
   return 0;
 }
 
-void input() { 
+void input() {
   cin >> n >> k;
-  int m,v;
+  int m, v;
 
-  for(int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; ++i) {
     scanf("%d %d", &m, &v);
     gemstones[i].first = m;
     gemstones[i].second = v;
   }
 
   int temp;
-  for(int i = 0; i < k; ++i) {
+  for (int i = 0; i < k; ++i) {
     scanf("%d", &bagArr[i]);
   }
 
-  return; 
+  return;
 }
 
-void solve() { 
+void solve() {
   sort(gemstones, gemstones + n);
   sort(bagArr, bagArr + k);
 
   ll valueSum = 0;
   priority_queue<int> q;
 
-  for(int i = 0, j = 0; i < k; ++i) {
+  for (int i = 0, j = 0; i < k; ++i) {
     // 현재 가방에 들어가는 모든 보석 넣기
-    while(j < n && gemstones[j].first <= bagArr[i]) {
+    while (j < n && gemstones[j].first <= bagArr[i]) {
       q.push(gemstones[j].second);
       j++;
     }
@@ -65,6 +65,5 @@ void solve() {
   }
 
   cout << valueSum;
-  return; 
+  return;
 }
-
