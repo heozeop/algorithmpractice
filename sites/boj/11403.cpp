075fd3931e6920065d2e12a55273e6f659287425
Lee seung chan <dltmdcks702@gmail.com>
2023-06-10 14:36:26 +0900

    solve: boj silver 1 11403
    1. 계속 바로 풀려고 한다.
    1. 특히 아는 거일 수록 그런거 같다.
    1. 쓰고 푸는데도 실수가 있는데 조심하자.

---
+++ sites/boj/11403.cpp
@@ -0,0 +1,81 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+const int VISITED = 1;
+const int MAX_N = 100;
+
+int START_IDX, END_IDX;
+
+int n;
+int directedGraph[MAX_N][MAX_N];
+int visited[MAX_N][MAX_N];
+
+void input();
+void solve();
+void dfs(int from, int to);
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> n;
+  START_IDX = 0;
+  END_IDX = n;
+
+  for(int i = START_IDX; i < END_IDX; ++i) {
+    for(int j = START_IDX; j < END_IDX; ++j) {
+      cin >> directedGraph[i][j];
+    }
+  }
+  return;
+}
+
+void solve() { 
+  for(int i = START_IDX; i < END_IDX; ++i) {
+    for(int j = START_IDX; j < END_IDX; ++j) {
+      if(i == j) {
+        continue;
+      }
+
+      if (!directedGraph[i][j]) {
+        continue;
+      }
+
+      dfs(i,j);
+    }
+  }
+
+  for(int i = START_IDX; i < END_IDX; ++i) {
+    for(int j = START_IDX; j < END_IDX; ++j) {
+      cout << visited[i][j] << " ";
+    }
+    cout << '\n';
+  }
+  
+  return; 
+}
+
+void dfs(int from, int to) {
+  if(visited[from][to] == VISITED) {
+    return;
+  }
+
+  visited[from][to] = VISITED;
+
+  for(int i = START_IDX; i < END_IDX; ++i) {
+    if (!directedGraph[to][i]) {
+      continue;
+    }
+
+    dfs(from, i);
+  }
+}
+
