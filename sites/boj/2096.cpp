32f29dde640a008f73bd76b4d79ccce4844f733c
Lint Action <lint-action@samuelmeuli.com>
2023-06-11 05:31:44 +0000

    Fix code style issues with clang_format

--- sites/boj/2096.cpp
+++ sites/boj/2096.cpp
@@ -21,38 +21,38 @@ int main(void) {
   return 0;
 }
 
-void input() { 
+void input() {
   cin >> n;
 
-  for(int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; ++i) {
     cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
   }
 
-  return; 
+  return;
 }
 
-void solve() { 
+void solve() {
   // max
-  for(int i = 0; i < 6; ++i) {
+  for (int i = 0; i < 6; ++i) {
     dp[i] = arr[0][i];
-    dp[i] = arr[0][i%3];
+    dp[i] = arr[0][i % 3];
   }
 
-  int a,b,c;
-  for(int i = 1; i < n; ++i) {
+  int a, b, c;
+  for (int i = 1; i < n; ++i) {
     a = dp[0], b = dp[1], c = dp[2];
-    dp[0] = max(a, b) + arr[i][0]; 
-    dp[1] = max(max(a,b), c) + arr[i][1]; 
-    dp[2] = max(b,c) + arr[i][2]; 
+    dp[0] = max(a, b) + arr[i][0];
+    dp[1] = max(max(a, b), c) + arr[i][1];
+    dp[2] = max(b, c) + arr[i][2];
 
     a = dp[3], b = dp[4], c = dp[5];
-    dp[3] = min(a,b) + arr[i][0]; 
-    dp[4] = min(min(a,b), c) + arr[i][1]; 
-    dp[5] = min(b,c) + arr[i][2]; 
+    dp[3] = min(a, b) + arr[i][0];
+    dp[4] = min(min(a, b), c) + arr[i][1];
+    dp[5] = min(b, c) + arr[i][2];
   }
 
   cout << max(max(dp[0], dp[1]), dp[2]) << " ";
   cout << min(min(dp[3], dp[4]), dp[5]);
 
-  return; 
+  return;
 }
