8eedf012d900b99390279ccb9db060fa49b34191
Lee seung chan <dltmdcks702@gmail.com>
2023-06-13 20:24:31 +0900

    copy: boj gold 4 12851
    1. 이번에도 역시 지식에 잡아 먹혔다.
    1. 개수를 세는 조건을 보기보다 개수를 저장할 수 있는 요건을 봤다.
    1. dp로 풀이하려다 보니 쉽게 접근하지 못했다.
    1. 아쉽다.

---
+++ sites/boj/12851.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+const int MAX_N = 200001;
+
+int n, k;
+bool visited[MAX_N];
+
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> n >> k;
+  return;
+}
+
+void solve() {
+  queue<pair<int, int>> q;
+  q.push({0, n});
+  visited[n] = true;
+
+  int fastestTime = INT_MAX;
+  int timeInSec, currentPos;
+  int methodCount = 0;
+  while (!q.empty()) {
+    timeInSec = q.front().first;
+    currentPos = q.front().second;
+    q.pop();
+
+    if (timeInSec > fastestTime) {
+      continue;
+    }
+
+    if (currentPos == k) {
+      fastestTime = timeInSec;
+      methodCount += 1;
+      continue;
+    }
+
+    int nextPos = currentPos - 1;
+    if (nextPos >= 0 && !visited[nextPos]) {
+      q.push({timeInSec + 1, nextPos});
+    }
+
+    nextPos = currentPos + 1;
+    if (nextPos <= 2 * k && !visited[nextPos]){
+      q.push({timeInSec + 1, nextPos});
+    }
+
+    nextPos = currentPos * 2;
+    if (nextPos <= 2 * k && !visited[nextPos]){
+      q.push({timeInSec + 1, nextPos});
+    }
+  }
+
+  cout << fastestTime << "\n" << methodCount;
+
+  return;
+}
