d7929d79f069db5479fb80810623c8a99d827d52
Lee seung chan <dltmdcks702@gmail.com>
2023-06-23 08:35:51 +0900

    solve: boj gold 5 2493

---
+++ sites/boj/2493.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+const int MAX_N = 500001;
+
+int n;
+int arr[MAX_N];
+
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> n;
+  for (int i = 0; i < n; ++i) {
+    cin >> arr[i];
+  }
+  return;
+}
+
+void solve() {
+  vector<int> temp;
+  for (int i = 0; i < n; ++i) {
+    int returnIndex = 0;
+
+    while(!temp.empty()) {
+      if (arr[temp.back()] < arr[i]) {
+        temp.pop_back();
+      } else {
+        returnIndex = temp.back() + 1;
+        break;
+      }
+    }
+
+    cout << returnIndex << " ";
+    temp.push_back(i);
+
+  }
+  return;
+}
