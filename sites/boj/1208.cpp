e5dddc36b3531a663049f2001e710b1acd5d442e
Lee seung chan <dltmdcks702@gmail.com>
2023-06-24 09:19:37 +0900

    copy: boj gold 1 1208
    1. 모든 경우의 수
    1. 반절 나누기
    1. 아까 내껀 왜 안되었나.

---
+++ sites/boj/1208.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+#include <map>
+
+using namespace std;
+const int MAX_N = 40;
+typedef long long ll;
+
+int n, s;
+int arr[MAX_N];
+map<ll, ll> sumVal;
+ll countOfS;
+
+void input();
+void solve();
+void leftStore(int curIdx, ll sum);
+void rightStore(int curIdx, ll sum);
+
+int main(void) {
+  input();
+  solve();
+
+  return 0;
+}
+
+void input() { 
+  cin >> n >> s;
+  for(int i = 0; i < n; ++i) {
+    cin >> arr[i];
+  }
+
+  return; 
+}
+
+void solve() { 
+  leftStore(0, 0);
+  rightStore(n/2, 0);
+
+  if(s == 0) {
+    countOfS -= 1;
+  }
+
+  cout << countOfS;
+
+  return; 
+}
+
+void leftStore(int curIdx, ll sum) {
+  if(curIdx == n/2) {
+    sumVal[sum]++;
+    return;
+  }
+
+  leftStore(curIdx + 1, sum);
+  leftStore(curIdx + 1, sum + arr[curIdx]);
+}
+
+void rightStore(int curIdx, ll sum) {
+  if(curIdx == n) {
+    countOfS += sumVal[s - sum];
+    return;
+  }
+
+  rightStore(curIdx + 1, sum);
+  rightStore(curIdx + 1, sum + arr[curIdx]);
+}
+
