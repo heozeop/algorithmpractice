937389b7f59bdec959e2c5991bedd253a685fa9d
Lee seung chan <dltmdcks702@gmail.com>
2023-06-27 08:48:19 +0900

    copy: boj gold 5 1068
    1. 나는 개똥벌레
    1. 모르겠다.

---
+++ sites/boj/1068.cpp
@@ -0,0 +1,69 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector<int> parent[51];
+int leafCnt, rootIdx;
+
+void input();
+void solve();
+void dfs(int cur);
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> n;
+
+  int p;
+  for(int i = 0; i < n; ++i) {
+    cin >> p;
+    if (p == -1) {
+      rootIdx = i;
+      continue;
+    }
+
+    parent[p].push_back(i);
+  }
+
+  cin >> m;
+  return; 
+}
+
+void solve() { 
+  if(rootIdx == m) {
+    cout << 0;
+    return;
+  }
+
+
+  dfs(rootIdx);
+  cout << leafCnt;
+
+  return; 
+}
+
+void dfs(int cur) {
+  bool isChildrenExist = false;
+
+  for(int i : parent[cur]) {
+    if (i == m) {
+      continue;
+    }
+
+    dfs(i);
+    isChildrenExist = true;
+  }
+
+  if(!isChildrenExist) {
+    leafCnt += 1;
+  }
+}
