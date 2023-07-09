279b7081969e110f52beb83175e0dbf029e3e2f4
Lee seung chan <dltmdcks702@gmail.com>
2023-07-09 20:54:12 +0900

    solvE: boj bronze 5 10807

---
+++ sites/boj/10807.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int n;
+int t;
+int arr[100];
+
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> n;
+  for(int i = 0; i < n; ++i) {
+    cin >> arr[i];
+  }
+  cin >> t;
+  return; 
+}
+
+void solve() { 
+  int count = 0;
+  for(int i = 0; i < n; ++i) {
+    if(arr[i] == t){
+      count += 1;
+    }
+  }
+
+  cout << count;
+
+  return;
+}
