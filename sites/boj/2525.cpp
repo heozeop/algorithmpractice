d2a242088417576142717a3515198e44057a5dff
Lee seung chan <dltmdcks702@gmail.com>
2023-07-10 09:40:54 +0900

    solve: boj bronze 3 2525

---
+++ sites/boj/2525.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int h, m, t;
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> h >> m >> t;
+  return; 
+}
+
+void solve() { 
+  m += t;
+
+  h += m / 60;
+  m = m % 60;
+  h %= 24;
+
+  cout << h << " " << m;
+  return; 
+}
