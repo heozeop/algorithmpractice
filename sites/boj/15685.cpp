09b31a291158ff44d5cae8b891987bbcac474c65
Lint Action <lint-action@samuelmeuli.com>
2023-07-10 10:45:14 +0000

    Fix code style issues with clang_format

--- sites/boj/15685.cpp
+++ sites/boj/15685.cpp
@@ -2,28 +2,27 @@
 #include <iostream>
 #include <limits.h>
 #include <queue>
-#include <vector>
 #include <set>
+#include <vector>
 
 using namespace std;
 const int MAX_SIZE = 101;
 const int DIRECTION[4][2] = {
-  {0, 1}, // R
-  {-1, 0}, // T
-  {0, -1}, // L
-  {1, 0},  // B
+    {0, 1},  // R
+    {-1, 0}, // T
+    {0, -1}, // L
+    {1, 0},  // B
 };
 
-set<pair<int,int>> s;
+set<pair<int, int>> s;
 int n;
 
 void input();
 void solve();
-void findAndSetDragonCurve(int,int,int,int);
+void findAndSetDragonCurve(int, int, int, int);
 void printDragonRectangle();
-void turnVector(vector<pair<int,int>>& v);
+void turnVector(vector<pair<int, int>> &v);
 bool isAllInSet(int, int);
-
 
 int main(void) {
   input();
@@ -31,41 +30,41 @@ int main(void) {
   return 0;
 }
 
-void input() { 
+void input() {
   cin >> n;
-  int x,y,d,g;
-  for(int i = 0; i< n; ++i) {
+  int x, y, d, g;
+  for (int i = 0; i < n; ++i) {
     cin >> y >> x >> d >> g;
 
     findAndSetDragonCurve(x, y, d, g);
   }
-  return; 
+  return;
 }
 
-void solve() { 
+void solve() {
   printDragonRectangle();
-  return; 
+  return;
 }
 
 void findAndSetDragonCurve(int x, int y, int d, int g) {
-  vector<pair<int,int>> v;
-  v.push_back({x, y}); 
+  vector<pair<int, int>> v;
+  v.push_back({x, y});
   v.push_back({x + DIRECTION[d][0], y + DIRECTION[d][1]});
 
-  for(int i = 1; i <= g; ++i) {
-     turnVector(v);
+  for (int i = 1; i <= g; ++i) {
+    turnVector(v);
   }
 
-  for(auto i : v) {
+  for (auto i : v) {
     s.insert(i);
   }
 }
 
 void printDragonRectangle() {
   int count = 0;
-  for(int i = 0; i < MAX_SIZE - 1; ++i) {
-    for(int j = 0; j < MAX_SIZE - 1; ++j) {
-      if (isAllInSet(i,j)) {
+  for (int i = 0; i < MAX_SIZE - 1; ++i) {
+    for (int j = 0; j < MAX_SIZE - 1; ++j) {
+      if (isAllInSet(i, j)) {
         count += 1;
       }
     }
@@ -75,31 +74,32 @@ void printDragonRectangle() {
 }
 
 bool isAllInSet(int x, int y) {
-  if(s.find({x, y}) == s.end()) {
+  if (s.find({x, y}) == s.end()) {
     return false;
   }
-  if(s.find({x, y + 1}) == s.end()) {
+  if (s.find({x, y + 1}) == s.end()) {
     return false;
   }
-  if(s.find({x + 1, y}) == s.end()) {
+  if (s.find({x + 1, y}) == s.end()) {
     return false;
   }
-  if(s.find({x + 1, y + 1}) == s.end()) {
+  if (s.find({x + 1, y + 1}) == s.end()) {
     return false;
   }
 
   return true;
 }
 
-void turnVector(vector<pair<int,int>>& v) {
-  pair<int,int> center = v.back();
-  
-  for(int i = v.size() - 2; i >= 0; --i) {
+void turnVector(vector<pair<int, int>> &v) {
+  pair<int, int> center = v.back();
+
+  for (int i = v.size() - 2; i >= 0; --i) {
     int ax = v[i].first - center.first;
     int ay = v[i].second - center.second;
 
     v.push_back({
-        ay + center.first, -ax + center.second,
+        ay + center.first,
+        -ax + center.second,
     });
   }
 }
