4cee7638ca4265db27324cb63dfc5ae889387008
Lee seung chan <dltmdcks702@gmail.com>
2023-07-12 05:15:18 +0900

    copy: boj bronze 2 1009
    1. 충격! 브론즈도 안된다.
    1. 값 범위에 대해서 이해를 잘 못하는 듯 하다.
    1. 케이스 나누기를 더 연습해야겠다.

---
+++ sites/boj/1009.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int t;
+
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> t;
+  return; 
+}
+
+void solve() { 
+  while(t--) {
+    long long a, b;
+    cin >> a >> b;
+
+    long long c = 1;
+    while(b--) {
+      c *= a;
+      c %= 10;
+    }
+
+    if(c == 0) {
+      cout << 10;
+    } else {
+      cout << c;
+    }
+    cout << '\n';
+  }
+
+  return; 
+}
