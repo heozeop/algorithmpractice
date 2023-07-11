45a55edf1e6e30a24e59627ccba42eca3cce98fe
Lee seung chan <dltmdcks702@gmail.com>
2023-07-12 05:59:56 +0900

    copy: boj gold 3 1937
    1. 패배감 든다.
    1. 되는 이유를 말로 설명하기 어렵다.
    1. 아쉽다.
    1. dp에 저장되는 결과는 특정 지점에서 탐색한 가장 오래 머문 시간이다.
    1. dfs로 이를 측정한다.
    1. 정말 내가 풀 수 있도록 노력해야 겠다.

--- sites/boj/1937.cpp
+++ sites/boj/1937.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 
 using namespace std;
+const int MAX_V = 1000001;
 const int MAX_N = 500;
 const int DIRECTION[4][2] = {
     {0, 1},
@@ -16,12 +17,11 @@ const int DIRECTION[4][2] = {
 
 int n;
 int arr[MAX_N][MAX_N];
-int mv[MAX_N][MAX_N];
+int dp[MAX_N][MAX_N];
 
 void input();
 void solve();
-void floydwashal();
-void printMax();
+int dfs(int x, int y);
 bool isInArea(int, int);
 
 int main(void) {
@@ -42,52 +42,43 @@ void input() {
 }
 
 void solve() {
-
-  floydwashal();
-  printMax();
-
-  return;
-}
-
-void floydwashal() {
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      mv[i][j] = 1;
-    }
-  }
-
-  for (int l = 0; l < 4; ++l) {
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < n; ++j) {
-        int nx, ny;
-        for (int k = 0; k < 4; ++k) {
-          nx = i + DIRECTION[k][0];
-          ny = j + DIRECTION[k][1];
-
-          if (!isInArea(nx, ny)) {
-            continue;
-          }
-
-          if (arr[i][j] >= arr[nx][ny]) {
-            continue;
-          }
-
-          mv[nx][ny] = max(mv[nx][ny], mv[i][j] + 1);
-        }
-      }
-    }
-  }
-}
-
-void printMax() {
   int maxVal = 0;
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      maxVal = max(maxVal, mv[i][j]);
+  for(int i= 0; i < n; ++i) {
+    for(int j = 0; j < n; ++j) {
+      maxVal = max(maxVal, dfs(i, j));
     }
   }
 
   cout << maxVal;
+  
+  return;
+}
+
+int dfs(int x, int y) {
+  if (dp[x][y]) {
+    return dp[x][y];
+  }
+
+  int nx,ny;
+
+  dp[x][y] = 1;
+  for(int i = 0; i < 4; ++i) {
+    nx = x + DIRECTION[i][0];
+    ny = y + DIRECTION[i][1];
+
+    if (!isInArea(nx, ny)) {
+      continue;
+    }
+
+    if (arr[nx][ny] <= arr[x][y]) {
+      continue;
+    }
+
+    dp[x][y] = max(dp[x][y], dfs(nx, ny) + 1);
+  }
+
+  return dp[x][y];
 }
 
 bool isInArea(int x, int y) { return x >= 0 && y >= 0 && x < n && y < n; }
+
