8756315db843e1c4548acb85fcd49ca5bf062d09
Lee seung chan <dltmdcks702@gmail.com>
2023-07-14 06:00:56 +0900

    fail: boj gold 4 1915
    1. 내가 문제를 되는 대로 푼다는 느낌을 받았다.
    1. 감이 없거나 방법을 모르는 것 같다.
    1. 문제 풀이 책을 일단 먼저 확인해 보고 접근 방법을 정의해 봐야겠다.

---
+++ sites/boj/1915.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+const int MAX_N = 1000;
+
+int n,m;
+int arr[MAX_N][MAX_N];
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> n >> m;
+  for(int i = 0; i < n; ++i){
+    for(int j = 0; j < m; ++j) {
+      cin >> arr[i][j];
+    }
+  }
+  return; 
+}
+
+void solve() { 
+  vector<int> row(n, 0);
+  vector<int> col(m,0);
+
+  for(int i = 0; i < n; ++i) {
+    int localMax = 0;
+    for(int j = 0; j < m; ++j) {
+      if (arr[i][j] == 1) {
+        localMax += 1;
+      } else {
+        row[i] = max(row[i], localMax);
+        localMax = 0;
+      }
+    }
+    row[i] = max(row[i], localMax);
+  }
+
+  for(int i = 0; i < m; ++i) {
+    int localMax = 0;
+    for(int j = 0; j < n; ++j) {
+      if(arr[j][i] == 1){
+        localMax += 1;
+      } else {
+        col[i] = max(col[i], localMax);
+        localMax = 0;
+      }
+    }
+    col[i] = max(col[i], localMax);
+  }
+  return; 
+}
