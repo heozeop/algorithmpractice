4cda0bc7e343b7b7721664d6a45ce7925edca51b
Lee seung chan <dltmdcks702@gmail.com>
2023-07-16 05:11:48 +0900

    copy: boj gold 4 5052
    1. 논리적으로 BF 접근 후 Trie까진 생각했다.
    1. Trie 구현에서 막혔다.
    1. 중간에 종료가 나옴 검증을 어떻게 할지 생각하다 그런 듯 하다.
    1. 결국 답을 봤는데, 중간에 있는지 검사하면 되는 거였다.

---
+++ sites/boj/5052.cpp
@@ -0,0 +1,99 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+const int MAX_N = 10000;
+
+class Trie {
+	
+	bool isFinish;
+	Trie *Node[10];
+public:
+	Trie() {
+		isFinish = false;
+		for (int i = 0; i < 10; i++) {
+			Node[i] = NULL;
+		}
+	}
+
+	void insert(string str, int idx) {
+		if (idx == str.size()) {
+			isFinish = true;
+			return;
+		}
+
+		int cur = str[idx] - '0';
+		if (Node[cur] == NULL) {
+      Node[cur] = new Trie();
+    }
+
+		Node[cur]->insert(str, idx + 1);
+	}
+
+	bool isPrefixExist(string str, int idx) {
+		if (str.size() == idx) {
+			return false;
+		}
+
+    // 중간 종료 존재
+		if (isFinish) {
+      return true;
+    }
+
+		int cur = str[idx] - '0';
+		if (Node[cur] == NULL) {
+      return false;
+    }
+
+		return Node[cur]->isPrefixExist(str, idx + 1);
+	}
+};
+
+int t,n;
+string arr[MAX_N];
+
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> t;
+  return; 
+}
+
+void solve() { 
+  while(t--) {
+    Trie head;
+
+    cin >> n;
+    for(int i = 0; i < n; ++i) {
+      cin >> arr[i];
+      head.insert(arr[i], 0);
+    }
+
+    bool isPrefixExist = false;
+    for(int i = 0; i < n; ++i) {
+      if(head.isPrefixExist(arr[i], 0)){
+        isPrefixExist = true;
+        break;
+      }
+    }
+
+    if(isPrefixExist) {
+      cout << "NO";
+    } else {
+      cout << "YES";
+    }
+
+    cout << '\n';
+  }
+  return; 
+}
