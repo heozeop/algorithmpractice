d01cf581d240a250b4765bb55f130b6822302b07
Lint Action <lint-action@samuelmeuli.com>
2023-07-15 20:14:19 +0000

    Fix code style issues with clang_format

--- sites/boj/5052.cpp
+++ sites/boj/5052.cpp
@@ -8,51 +8,52 @@ using namespace std;
 const int MAX_N = 10000;
 
 class Trie {
-	
-	bool isFinish;
-	Trie *Node[10];
+
+  bool isFinish;
+  Trie *Node[10];
+
 public:
-	Trie() {
-		isFinish = false;
-		for (int i = 0; i < 10; i++) {
-			Node[i] = NULL;
-		}
-	}
+  Trie() {
+    isFinish = false;
+    for (int i = 0; i < 10; i++) {
+      Node[i] = NULL;
+    }
+  }
 
-	void insert(string str, int idx) {
-		if (idx == str.size()) {
-			isFinish = true;
-			return;
-		}
+  void insert(string str, int idx) {
+    if (idx == str.size()) {
+      isFinish = true;
+      return;
+    }
 
-		int cur = str[idx] - '0';
-		if (Node[cur] == NULL) {
+    int cur = str[idx] - '0';
+    if (Node[cur] == NULL) {
       Node[cur] = new Trie();
     }
 
-		Node[cur]->insert(str, idx + 1);
-	}
+    Node[cur]->insert(str, idx + 1);
+  }
 
-	bool isPrefixExist(string str, int idx) {
-		if (str.size() == idx) {
-			return false;
-		}
-
-    // 중간 종료 존재
-		if (isFinish) {
-      return true;
-    }
-
-		int cur = str[idx] - '0';
-		if (Node[cur] == NULL) {
+  bool isPrefixExist(string str, int idx) {
+    if (str.size() == idx) {
       return false;
     }
 
-		return Node[cur]->isPrefixExist(str, idx + 1);
-	}
+    // 중간 종료 존재
+    if (isFinish) {
+      return true;
+    }
+
+    int cur = str[idx] - '0';
+    if (Node[cur] == NULL) {
+      return false;
+    }
+
+    return Node[cur]->isPrefixExist(str, idx + 1);
+  }
 };
 
-int t,n;
+int t, n;
 string arr[MAX_N];
 
 void input();
@@ -64,30 +65,30 @@ int main(void) {
   return 0;
 }
 
-void input() { 
+void input() {
   cin >> t;
-  return; 
+  return;
 }
 
-void solve() { 
-  while(t--) {
+void solve() {
+  while (t--) {
     Trie head;
 
     cin >> n;
-    for(int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
       cin >> arr[i];
       head.insert(arr[i], 0);
     }
 
     bool isPrefixExist = false;
-    for(int i = 0; i < n; ++i) {
-      if(head.isPrefixExist(arr[i], 0)){
+    for (int i = 0; i < n; ++i) {
+      if (head.isPrefixExist(arr[i], 0)) {
         isPrefixExist = true;
         break;
       }
     }
 
-    if(isPrefixExist) {
+    if (isPrefixExist) {
       cout << "NO";
     } else {
       cout << "YES";
@@ -95,5 +96,5 @@ void solve() {
 
     cout << '\n';
   }
-  return; 
+  return;
 }
