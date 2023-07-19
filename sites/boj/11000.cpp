a3223b68df1e1ffccc07ebb5e832cc8e8f01b250
Lee seung chan <dltmdcks702@gmail.com>
2023-07-20 07:40:43 +0900

    copy: boj gold 5 11000
    1. 로직 자체는 맞았으나 시간초과 오류로 20분을 넘겼다.
    1. queue에 넣고 빼는데 그것을 방 사이즈라고 생각을 못했다.
    1. 앞으로는 우선순위 큐쓸 때 자리 바꿈에 대해서 잘 생각해 봐야 겠다.
    1. vector로 INDEX접근해 바꾸는 것과 크게 차이 없음을 인지 하자

---
+++ sites/boj/11000.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<pair<int,int>> v;
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() {
+  cin >> n;
+
+  int s, e;
+  for(int i = 0; i < n; ++i) {
+    cin >> s >> e;
+    v.push_back({s,e});
+  }
+
+  sort(v.begin(), v.end());
+
+  return;
+}
+
+void solve() { 
+  priority_queue<int, vector<int>, greater<int>> lectureRoom;
+  lectureRoom.push(0);
+  int lectureRoomCount = 1;
+
+  for(int i = 0; i < n; ++i) {
+    lectureRoom.push(v[i].second);
+    if (lectureRoom.top() <= v[i].first) {
+      lectureRoom.pop();
+    }
+  }
+
+  cout << lectureRoom.size();
+
+  return; 
+}
