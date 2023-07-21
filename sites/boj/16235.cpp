0d763aad84ebc5502d91dd084ed796e24bfc0fc4
Lee seung chan <dltmdcks702@gmail.com>
2023-07-21 20:07:37 +0900

    copy: boj gold 3 16235
    1. deque를 이용해서 풀명 쉽게 풀리는 문제였다.
    1. 시간 복잡도 계산 없이 그냥 priority queue를 이용하니 안풀리는 것
       같다.
    1. 다시 한번 풀어봐야 하는 문제로, 시간 복잡도를 제대로 계산하는 습관을
       들여야 겠다.

--- sites/boj/16235.cpp
+++ sites/boj/16235.cpp
@@ -1,35 +1,28 @@
 #include <algorithm>
+#include <cstring>
 #include <iostream>
 #include <limits.h>
 #include <queue>
 #include <vector>
-#include <cstring>
 
 using namespace std;
 
 const int DIRECTION[8][2] = {
-  {0,1},
-  {0,-1},
-  {1,1},
-  {-1,1},
-  {-1,-1},
-  {1,-1},
-  {-1,0},
-  {1,0},
+    {0, 1}, {0, -1}, {1, 1}, {-1, 1}, {-1, -1}, {1, -1}, {-1, 0}, {1, 0},
 };
 const int MAX_N = 11;
 
 int n, m, k;
 int arr[MAX_N][MAX_N];
 int a[MAX_N][MAX_N];
-priority_queue<pair<int, pair<int,int>>> q;
+deque<int> tree[MAX_N][MAX_N];
 
 void input();
 void solve();
 void springSummerFall();
 void winter();
 void printTrees();
-bool isInArea(int,int);
+bool isInArea(int, int);
 
 int main(void) {
   input();
@@ -37,108 +30,125 @@ int main(void) {
   return 0;
 }
 
-void input() { 
+void input() {
   cin >> n >> m >> k;
 
-  for(int i = 1; i <= n; ++i) {
-    for(int j= 1; j <= n; ++j) {
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= n; ++j) {
       arr[i][j] = 5;
       cin >> a[i][j];
     }
   }
 
-  int x,y,k;
-  for(int i = 0; i < m; ++i) {
+  int x, y, k;
+  for (int i = 0; i < m; ++i) {
     cin >> x >> y >> k;
-    q.push({k, {x,y}});
+    tree[x][y].push_back(k);
   }
-  
 
-  return; 
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      sort(tree[i][j].begin(), tree[i][j].end());
+    }
+  }
+
+  return;
 }
 
-void solve() { 
-  while(k--) {
+void solve() {
+  while (k--) {
     springSummerFall();
     winter();
   }
 
   printTrees();
-  return; 
+  return;
 }
 
 void springSummerFall() {
-  vector<pair<int,pair<int,int>>> normal;
-  vector<pair<int,pair<int,int>>> died;
-  vector<pair<int,pair<int,int>>> grow;
 
-  // spring
-  int x, y, k;
-  while(!q.empty()) {
-    auto temp = q.top();
-    q.pop();
-
-    k = temp.first;
-    x = temp.second.first;
-    y = temp.second.second;
-    
-    if(arr[x][y] < k) {
-      died.push_back({k, {x, y}});
-      continue;
-    }
-
-    arr[x][y] -= k;
-    if((k + 1) % 5 == 0) {
-      grow.push_back({k + 1, {x, y}});
-    } else {
-      normal.push_back({k + 1, {x, y}});
-    }
-  }
-
-  for(auto temp : normal) {
-    q.push(temp);
-  }
-
-  // summer
-  for(auto temp : died) {
-    k = temp.first;
-    x = temp.second.first;
-    y = temp.second.second;
-
-    arr[x][y] += k / 2;
-  }
-
-  // fall
-  for(auto temp : grow) {
-    q.push(temp);
-
-    int nx, ny;
-    for(int i = 0; i < 8; ++i) {
-      nx = temp.second.first + DIRECTION[i][0];
-      ny = temp.second.second + DIRECTION[i][1];
-
-      if(!isInArea(nx, ny)) {
+  for(int i = 1; i <= n; ++i) {
+    for(int j = 1; j <= n; ++j) {
+      if(tree[i][j].size() < 1) {
         continue;
       }
 
-      q.push({1, {nx, ny}});
+      int idx = -1;
+      int size = tree[i][j].size();
+
+      // spring
+      for(int k = 0; k < size; ++k) {
+        if(arr[i][j] < tree[i][j][k]) {
+          idx = k;
+          break;
+        } else {
+          arr[i][j] -= tree[i][j][k];
+          tree[i][j][k] += 1;
+        }
+      }
+
+      if (idx < 0) {
+        continue;
+      }
+
+      // summer
+      for(int k = size - 1; k >= idx; --k) {
+        arr[i][j] += tree[i][j][k] / 2;
+        tree[i][j].pop_back();
+      }
+    }
+  }
+
+  // fall
+  for(int i = 1; i <= n; ++i) {
+    for(int j = 1; j <= n; ++j) {
+      int size = tree[i][j].size();
+
+      if(size < 1) {
+        continue;
+      }
+
+      for(int k = 0; k < size; ++k) {
+        if(tree[i][j][k] % 5 != 0) {
+          continue;
+        }
+
+        int nx,ny;
+        for(int l = 0; l < 8; ++l) {
+          nx = i + DIRECTION[l][0];
+          ny = j + DIRECTION[l][1];
+
+          if(!isInArea(nx,ny)) {
+            continue;
+          }
+
+          tree[nx][ny].push_front(1);
+        }
+      }
     }
   }
 }
 
 void winter() {
-  for(int i = 1; i <= n; ++i) {
-    for(int j = 1; j <= n; ++j) {
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= n; ++j) {
       arr[i][j] += a[i][j];
     }
   }
 }
 
 void printTrees() {
-  cout << q.size();
+  int ans = 0;
+
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      if (tree[i][j].size() > 0) {
+        ans += tree[i][j].size();
+      }
+    }
+  }
+
+  cout << ans;
 }
 
-bool isInArea(int x, int y) {
-  return x > 0 && y > 0 && x <= n && y <= n;
-}
-
+bool isInArea(int x, int y) { return x > 0 && y > 0 && x <= n && y <= n; }
