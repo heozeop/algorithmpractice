20f457131aad35e0b95507b362ee8f23d9796e07
Lint Action <lint-action@samuelmeuli.com>
2023-07-23 11:12:33 +0000

    Fix code style issues with clang_format

--- sites/boj/1600.cpp
+++ sites/boj/1600.cpp
@@ -8,21 +8,14 @@ using namespace std;
 const int MAX_N = 200;
 const int OBSTICLE = 1;
 const int DIRECTION[4][2] = {
-  {0,1},
-  {0,-1},
-  {1,0},
-  {-1,0},
+    {0, 1},
+    {0, -1},
+    {1, 0},
+    {-1, 0},
 };
 
 const int HORSE_MOVE[8][2] = {
-  {-1, -2},
-  {1, -2},
-  {-1, 2},
-  {1, 2},
-  {2, 1},
-  {2, -1},
-  {-2, 1},
-  {-2, -1},
+    {-1, -2}, {1, -2}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1},
 };
 
 int k, h, w;
@@ -37,7 +30,7 @@ struct Move {
 
 void input();
 void solve();
-bool isInArea(int,int);
+bool isInArea(int, int);
 
 int main(void) {
   input();
@@ -45,17 +38,17 @@ int main(void) {
   return 0;
 }
 
-void input() { 
+void input() {
   cin >> k >> w >> h;
-  for(int i = 0; i < h; ++i) {
-    for(int j = 0; j < w; ++j) {
+  for (int i = 0; i < h; ++i) {
+    for (int j = 0; j < w; ++j) {
       cin >> arr[i][j];
     }
   }
-  return; 
+  return;
 }
 
-void solve() { 
+void solve() {
   queue<Move> q;
   Move m = {0, k, 0, 0};
   q.push(m);
@@ -63,7 +56,7 @@ void solve() {
   v[k][0][0] = 1;
 
   int nx, ny;
-  while(!q.empty()) {
+  while (!q.empty()) {
     m = q.front();
     q.pop();
 
@@ -72,54 +65,52 @@ void solve() {
       return;
     }
 
-    for(int i = 0; i < 4; ++i) {
+    for (int i = 0; i < 4; ++i) {
       nx = m.x + DIRECTION[i][0];
       ny = m.y + DIRECTION[i][1];
 
-      if(!isInArea(nx,ny)) {
+      if (!isInArea(nx, ny)) {
         continue;
       }
 
-      if(arr[nx][ny] == OBSTICLE) {
+      if (arr[nx][ny] == OBSTICLE) {
         continue;
       }
 
-      if(v[m.k][nx][ny]) {
+      if (v[m.k][nx][ny]) {
         continue;
       }
 
       v[m.k][nx][ny] = 1;
-      q.push({m.cnt+1, m.k, nx, ny});
+      q.push({m.cnt + 1, m.k, nx, ny});
     }
 
     if (m.k < 1) {
       continue;
     }
 
-    for(int i = 0; i < 8; ++i) {
+    for (int i = 0; i < 8; ++i) {
       nx = m.x + HORSE_MOVE[i][0];
       ny = m.y + HORSE_MOVE[i][1];
 
-      if(!isInArea(nx,ny)) {
+      if (!isInArea(nx, ny)) {
         continue;
       }
 
-      if(arr[nx][ny] == OBSTICLE) {
+      if (arr[nx][ny] == OBSTICLE) {
         continue;
       }
 
-      if(v[m.k - 1][nx][ny]) {
+      if (v[m.k - 1][nx][ny]) {
         continue;
       }
       v[m.k - 1][nx][ny] = 1;
-      q.push({m.cnt+1, m.k - 1, nx, ny});
+      q.push({m.cnt + 1, m.k - 1, nx, ny});
     }
   }
 
   cout << -1;
-  return; 
+  return;
 }
 
-bool isInArea(int x, int y) {
-  return x >= 0 && y >= 0 && x < h && y < w;
-}
+bool isInArea(int x, int y) { return x >= 0 && y >= 0 && x < h && y < w; }
