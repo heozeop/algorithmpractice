2c79c0d7a41e62669e26270b33a5fb79bd3be1b2
Lee seung chan <dltmdcks702@gmail.com>
2023-07-31 08:23:20 +0900

    copy: boj gold 2 2437
    1. 답 배꼈다.
    1. 내가 풀 수 있을때까진 봉인
    1. 어차피 여기 풀면 읽어보려 하지 않을까 싶긴하다.
        1. 그래서 지움

---
+++ sites/boj/2437.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+const int MAX_N = 1001;
+
+int n;
+int arr[MAX_N];
+int minDp[MAX_N];
+int maxDp[MAX_N];
+
+
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> n;
+  for(int i = 0; i < n; ++i) {
+    cin >> arr[i];
+  }
+  
+  return; 
+}
+
+void solve() { 
+ return; 
+}
