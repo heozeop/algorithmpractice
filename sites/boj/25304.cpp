16cdb1f0724ef0087f5b1dfefddde5a5bead5285
Lint Action <lint-action@samuelmeuli.com>
2023-08-05 09:58:47 +0000

    Fix code style issues with clang_format

--- sites/boj/25304.cpp
+++ sites/boj/25304.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-int x,n,sum;
+int x, n, sum;
 
 void input();
 void solve();
@@ -17,23 +17,23 @@ int main(void) {
   return 0;
 }
 
-void input() { 
+void input() {
   cin >> x >> n;
 
-  int a,b;
-  for(int i = 0; i < n; ++i) {
+  int a, b;
+  for (int i = 0; i < n; ++i) {
     cin >> a >> b;
     sum += a * b;
   }
-  return; 
+  return;
 }
 
-void solve() { 
-  if(sum == x) {
+void solve() {
+  if (sum == x) {
     cout << "Yes";
   } else {
     cout << "No";
   }
 
-  return; 
+  return;
 }
--- sites/boj/2631.cpp
+++ sites/boj/2631.cpp
@@ -19,22 +19,22 @@ int main(void) {
   return 0;
 }
 
-void input() { 
+void input() {
   cin >> n;
-  for(int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; ++i) {
     cin >> arr[i];
   }
-  return; 
+  return;
 }
 
-void solve() { 
+void solve() {
   int maxVal = 0;
   vector<int> v;
 
   v.push_back(arr[0]);
 
-  for(int i = 1; i < n; ++i) {
-    if(v.back() < arr[i]) {
+  for (int i = 1; i < n; ++i) {
+    if (v.back() < arr[i]) {
       v.push_back(arr[i]);
     } else {
       auto temp = lower_bound(v.begin(), v.end(), arr[i]);
