15ecb09197e7a57cc41cc035085ff31db544a2dc
Lint Action <lint-action@samuelmeuli.com>
2023-08-05 10:55:42 +0000

    Fix code style issues with clang_format

--- sites/boj/11057.cpp
+++ sites/boj/11057.cpp
@@ -18,31 +18,31 @@ int main(void) {
   return 0;
 }
 
-void input() { 
+void input() {
   cin >> n;
-  return; 
+  return;
 }
 
-void solve() { 
-  for(int i = 0; i < 10; ++i) {
-    dp[1][i] = 1; 
+void solve() {
+  for (int i = 0; i < 10; ++i) {
+    dp[1][i] = 1;
   }
 
-  for(int i = 2; i <= n; ++i) {
-    for(int j = 0; j <= 9; ++j) {
-      for(int k = j; k <= 9; ++k) {
-        dp[i][j] += dp[i-1][k];
+  for (int i = 2; i <= n; ++i) {
+    for (int j = 0; j <= 9; ++j) {
+      for (int k = j; k <= 9; ++k) {
+        dp[i][j] += dp[i - 1][k];
         dp[i][j] %= 10007;
       }
     }
   }
 
   int sum = 0;
-  for(int i = 0; i <= 9; ++i) {
+  for (int i = 0; i <= 9; ++i) {
     sum += dp[n][i];
     sum %= 10007;
   }
 
   cout << sum;
-  return; 
+  return;
 }
