f8e2d6fd56c44690445cb9213768c9f7986a2203
Lee seung chan <dltmdcks702@gmail.com>
2023-08-05 19:54:16 +0900

    solve: boj silver 1 11057
    1. 숫자 세기롤 점화식을 세워서 풀었다.
    1. 약 20분 소요 했다.
    1. 몇개 해보면서 풀었다.
    1. 작게작게 해보면서 풀면 좋을 듯 하다.

---
+++ sites/boj/11057.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int n;
+int dp[1001][1001];
+
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> n;
+  return; 
+}
+
+void solve() { 
+  for(int i = 0; i < 10; ++i) {
+    dp[1][i] = 1; 
+  }
+
+  for(int i = 2; i <= n; ++i) {
+    for(int j = 0; j <= 9; ++j) {
+      for(int k = j; k <= 9; ++k) {
+        dp[i][j] += dp[i-1][k];
+        dp[i][j] %= 10007;
+      }
+    }
+  }
+
+  int sum = 0;
+  for(int i = 0; i <= 9; ++i) {
+    sum += dp[n][i];
+    sum %= 10007;
+  }
+
+  cout << sum;
+  return; 
+}
