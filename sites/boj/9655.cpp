bd738577253f4747f0c3035d46634e3849873691
Lint Action <lint-action@samuelmeuli.com>
2023-08-07 14:21:37 +0000

    Fix code style issues with clang_format

--- sites/boj/9655.cpp
+++ sites/boj/9655.cpp
@@ -17,26 +17,26 @@ int main(void) {
   return 0;
 }
 
-void input() { 
+void input() {
   cin >> n;
-  return; 
+  return;
 }
 
-void solve() { 
+void solve() {
   queue<pair<int, int>> q;
   q.push({1, 1});
   q.push({3, 1});
   dp[1] = 1;
   dp[2] = 2;
   dp[3] = 1;
-  for (int i = 4; i <= n; ++i ) {
+  for (int i = 4; i <= n; ++i) {
     dp[i] = min(dp[i - 1], dp[i - 3]) + 1;
   }
-  if(dp[n] % 2 == 1) {
+  if (dp[n] % 2 == 1) {
     cout << "SK";
   } else {
     cout << "CY";
   }
 
-  return; 
+  return;
 }
