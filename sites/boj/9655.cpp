2e00c8c582eebf315839979adb7de14b508c6d46
Lee seung chan <dltmdcks702@gmail.com>
2023-08-07 23:09:26 +0900

    copy: boj silver 5 9655
    1. 자존감의 하락을 불러온다.
    1. 게임 수를 기준으로 접근해 bfs로 탐색했다.
    1. 먼저 n-2 혹은 n-4에 도달하는 놈이 이기는 걸로 했다
        - 근데 틀림
    많이 풀어봐야 겠다.

---
+++ sites/boj/9655.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int n;
+int dp[1001];
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> n;
+  return; 
+}
+
+void solve() { 
+  queue<pair<int, int>> q;
+  q.push({1, 1});
+  q.push({3, 1});
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 1;
+  for (int i = 4; i <= n; ++i ) {
+    dp[i] = min(dp[i - 1], dp[i - 3]) + 1;
+  }
+  if(dp[n] % 2 == 1) {
+    cout << "SK";
+  } else {
+    cout << "CY";
+  }
+
+  return; 
+}
