3073244939610de7621e7656c878d5c1d743c0fc
Lint Action <lint-action@samuelmeuli.com>
2023-08-08 23:43:39 +0000

    Fix code style issues with clang_format

--- sites/boj/11048.cpp
+++ sites/boj/11048.cpp
@@ -11,7 +11,7 @@ int arr[MAX_NM][MAX_NM];
 int dp[MAX_NM][MAX_NM];
 bool visited[MAX_NM][MAX_NM];
 
-int n,m;
+int n, m;
 void input();
 void solve();
 void backtrack(int, int, int);
@@ -22,32 +22,32 @@ int main(void) {
   return 0;
 }
 
-void input() { 
+void input() {
   cin >> n >> m;
-  for(int i = 0; i < n; ++i) {
-    for(int j = 0; j < m; ++j) {
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
       cin >> arr[i][j];
     }
   }
-  return; 
+  return;
 }
 
-void solve() { 
+void solve() {
   dp[0][0] = arr[0][0];
-  for(int i = 1; i < n; ++i) {
-    dp[i][0] = dp[i-1][0] + arr[i][0];
+  for (int i = 1; i < n; ++i) {
+    dp[i][0] = dp[i - 1][0] + arr[i][0];
   }
 
-  for(int i = 1; i < m; ++i) {
-    dp[0][i] = dp[0][i-1] + arr[0][i];
+  for (int i = 1; i < m; ++i) {
+    dp[0][i] = dp[0][i - 1] + arr[0][i];
   }
 
-  for(int i = 1; i < n; ++i) { 
-    for(int j = 1; j < m; ++j) {
+  for (int i = 1; i < n; ++i) {
+    for (int j = 1; j < m; ++j) {
       dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + arr[i][j];
     }
   }
 
   cout << dp[n - 1][m - 1];
-  return; 
+  return;
 }
