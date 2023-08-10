d1664ccf8d3bd80b6089c2b2abcd62eaf17e1682
Lee seung chan <dltmdcks702@gmail.com>
2023-08-10 13:09:37 +0900

    solve: boj bronze 5 25314

---
+++ sites/boj/25314.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int n;
+
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> n;
+  return; 
+}
+
+void solve() { 
+  int s = n /4;
+  if(n% 4 > 0) {
+    s += 1;
+  }
+
+  for(int i = 0; i < s; ++i) {
+    cout << "long ";
+  }
+  cout << "int";
+  return; 
+}
