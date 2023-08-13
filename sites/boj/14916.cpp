d96f988b30ba139e8caac70031330878bcb74b90
Lint Action <lint-action@samuelmeuli.com>
2023-08-13 23:46:14 +0000

    Fix code style issues with clang_format

--- sites/boj/14916.cpp
+++ sites/boj/14916.cpp
@@ -1,9 +1,9 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <limits.h>
 #include <queue>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
@@ -18,20 +18,20 @@ int main(void) {
   return 0;
 }
 
-void input() { 
+void input() {
   cin >> n;
-  return; 
+  return;
 }
 
-void solve() { 
+void solve() {
   int answer = INT_MAX;
-  for(int i = 0; i <= n / 5; ++i) {
+  for (int i = 0; i <= n / 5; ++i) {
     int temp = n - i * 5;
-    int cnt2 = (temp/2);
+    int cnt2 = (temp / 2);
 
-    temp -= (cnt2) * 2;
+    temp -= (cnt2)*2;
 
-    if(temp > 0) {
+    if (temp > 0) {
       // 0 인 경우
       continue;
     }
@@ -39,12 +39,11 @@ void solve() {
     answer = min(answer, i + cnt2);
   }
 
-
-  if(answer== INT_MAX) {
+  if (answer == INT_MAX) {
     cout << -1;
     return;
   }
   cout << answer;
-  
-  return; 
+
+  return;
 }
