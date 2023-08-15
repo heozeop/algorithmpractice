59567d7f647e6fa908d2c206fd5295f7558f4776
Lee seung chan <dltmdcks702@gmail.com>
2023-08-15 11:55:06 +0900

    solve: boj silver 4 9656

---
+++ sites/boj/9656.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int n;
+void input();
+void solve();
+
+int main(void) {
+  input();
+  solve();
+  return 0;
+}
+
+void input() { 
+  cin >> n;
+  return; 
+}
+
+void solve() { 
+  if(n % 2) {
+    cout << "CY";
+  }else {
+    cout << "SK";
+  }
+  return; 
+}
